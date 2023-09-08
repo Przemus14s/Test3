4aeb1c0aa0c4e6a6949945af3def389b6742f11f
Przemus14s <przemekszynal5@gmail.com>
2023-09-08 09:34:44 +0200

    silnia

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+//        System.out.printf("Hello and welcome!");
+//
+//        for (int i = 1; i <= 5; i++) {
+//
+//
+//            System.out.println("i = " + i);
+        Scanner skaner = new Scanner(System.in);
+        System.out.println("Podaj liczbę");
+        int s = skaner.nextInt();
+        int silnia = 1;
 
-        for (int i = 1; i <= 5; i++) {
+        for(int i = 1;i<=s; i++){
+            silnia*=i;
 
 
-            System.out.println("i = " + i);
         }
+        System.out.println("Silnia wynosi " + silnia);
+
+
     }
 }
